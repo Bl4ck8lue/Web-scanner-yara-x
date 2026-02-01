5a0964e7f88065391c76d34f262eec8e3398d28d
Ilya Ilyukhin <ilya.ilyukhin12@yandex.ru>
2026-02-02 00:53:25 +0300

    Rewrite connect to DB using golang without python

--- main.go
+++ main.go
@@ -2,16 +2,22 @@ package main
 
 import (
 	"context"
+	"encoding/hex"
 	"encoding/json"
 	"errors"
 	"fmt"
 	"html/template"
 	"io"
+	"log"
 	"net/http"
 	"os"
 	"os/exec"
 	"path/filepath"
 	"time"
+
+	"crypto/md5"
+
+	"github.com/jackc/pgx/v5"
 )
 
 var (
@@ -26,10 +32,7 @@ var (
 	pythonBin   = "python3"
 	scriptPath  = "./scripts/analyze.py"
 	scriptPath1 = "./scripts/registr.py"
-<<<<<<< HEAD
-=======
 	scriptPath2 = "./scripts/signin.py"
->>>>>>> 67b6e45 (add js sign in btn and logic without adding cookie)
 	scanTimeout = 90 * time.Second
 )
 
@@ -39,10 +42,7 @@ func main() {
 	http.HandleFunc("/about", aboutHandler)
 	http.HandleFunc("/scan", scanHandler)
 	http.HandleFunc("/reg", regHandler)
-<<<<<<< HEAD
-=======
 	http.HandleFunc("/sign", signHandler)
->>>>>>> 67b6e45 (add js sign in btn and logic without adding cookie)
 
 	fmt.Println("Starting server at :8090")
 	if err := http.ListenAndServe(":8090", nil); err != nil && !errors.Is(err, http.ErrServerClosed) {
@@ -67,7 +67,37 @@ func aboutHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "This is the about page.")
 }
 
+func regInDB(name string, email string, pass string) {
+	conn, err := pgx.Connect(context.Background(), "postgres://ilya:4suh12iiyu@localhost:5432/web_scanner")
+	if err != nil {
+		log.Fatal("Hе удалось подключиться к БД:", err)
+	}
+	defer conn.Close(context.Background())
+
+	// Выполняем простой SQL-запрос
+	var greeting string
+	err = conn.QueryRow(context.Background(), "select count(*) from reg_users where email = $1", email).Scan(&greeting)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println(greeting)
+
+	if greeting != "0" {
+		log.Fatal("Error! Exists")
+	}
+
+	_, err = conn.Exec(context.Background(), "insert into reg_users (name, email, pass) values ($1, $2, md5($3))", name, email, pass)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// Выводим результат
+	fmt.Println("Added")
+}
+
 func regHandler(w http.ResponseWriter, r *http.Request) {
+
 	// Ограничить размер тела
 	r.Body = http.MaxBytesReader(w, r.Body, maxUploadSize)
 	if err := r.ParseMultipartForm(32 << 20); err != nil {
@@ -79,45 +109,84 @@ func regHandler(w http.ResponseWriter, r *http.Request) {
 	email := r.FormValue("email")
 	password := r.FormValue("p1")
 
-	// запустить python скрипт с таймаутом
-	ctx, cancel := context.WithTimeout(context.Background(), scanTimeout)
-	defer cancel()
+	regInDB(name, email, password)
 
-	cmd := exec.CommandContext(ctx, pythonBin, scriptPath1, name, email, password)
-	out, err := cmd.CombinedOutput()
+	/*
+		// запустить python скрипт с таймаутом
+		ctx, cancel := context.WithTimeout(context.Background(), scanTimeout)
+		defer cancel()
 
-	// timeout?
-	if ctx.Err() == context.DeadlineExceeded {
-		http.Error(w, "scan timeout", http.StatusGatewayTimeout)
-		return
-	}
-	fmt.Printf("registr output:\n%s\n", string(out))
+		cmd := exec.CommandContext(ctx, pythonBin, scriptPath1, name, email, password)
+		out, err := cmd.CombinedOutput()
 
-	// подготовить код возврата
-	exitCode := 0
-	if err != nil {
-		if ee, ok := err.(*exec.ExitError); ok {
-			exitCode = ee.ExitCode()
-		} else {
-			http.Error(w, "failed to run scanner: "+err.Error(), http.StatusInternalServerError)
+		// timeout?
+		if ctx.Err() == context.DeadlineExceeded {
+			http.Error(w, "scan timeout", http.StatusGatewayTimeout)
 			return
 		}
-	} else if cmd.ProcessState != nil {
-		exitCode = cmd.ProcessState.ExitCode()
-	}
+		fmt.Printf("registr output:\n%s\n", string(out))
 
-	// Формируем ответ — возвращаем stdout (ожидаем JSON от скрипта)
-	resp := map[string]any{
-		"exit_code": exitCode,
-		"output":    string(out),
-	}
-	//fmt.Println(string(out))
-	w.Header().Set("Content-Type", "application/json")
-	json.NewEncoder(w).Encode(resp)
+		// подготовить код возврата
+		exitCode := 0
+		if err != nil {
+			if ee, ok := err.(*exec.ExitError); ok {
+				exitCode = ee.ExitCode()
+			} else {
+				http.Error(w, "failed to run scanner: "+err.Error(), http.StatusInternalServerError)
+				return
+			}
+		} else if cmd.ProcessState != nil {
+			exitCode = cmd.ProcessState.ExitCode()
+		}
+
+		// Формируем ответ — возвращаем stdout (ожидаем JSON от скрипта)
+		resp := map[string]any{
+			"exit_code": exitCode,
+			"output":    string(out),
+		}
+		//fmt.Println(string(out))
+		w.Header().Set("Content-Type", "application/json")
+		json.NewEncoder(w).Encode(resp)*/
 }
 
-<<<<<<< HEAD
-=======
+func signInDB(email string, pass string) {
+	//postgres://ilya:4suh12iiyu@localhost:5432/web_scanner
+	//postrges://DB_USER:DB_PASSWORD@DB_HOST:5432/DB_NAME
+	// Устанавливаем соединение с базой данных
+	conn, err := pgx.Connect(context.Background(), "postgres://ilya:4suh12iiyu@localhost:5432/web_scanner")
+	if err != nil {
+		log.Fatal("не удалось подключиться к БД:", err)
+	}
+	defer conn.Close(context.Background())
+
+	// Выполняем простой SQL-запрос
+	var greeting string
+	err = conn.QueryRow(context.Background(), "select count(*) from reg_users where email = $1", email).Scan(&greeting)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	if greeting == "0" {
+		fmt.Println("Have to reg!")
+		log.Fatal("REG!")
+	}
+
+	var chpass string
+	err = conn.QueryRow(context.Background(), "select $1 from reg_users", pass).Scan(&chpass)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	mdpass := md5.Sum([]byte(pass))
+	passmd := hex.EncodeToString(mdpass[:])
+	if chpass == passmd {
+		fmt.Println("Hello, " + email)
+	}
+
+	// Выводим результат
+	fmt.Println(greeting)
+}
+
 func signHandler(w http.ResponseWriter, r *http.Request) {
 	// Ограничить размер тела
 	r.Body = http.MaxBytesReader(w, r.Body, maxUploadSize)
@@ -129,39 +198,41 @@ func signHandler(w http.ResponseWriter, r *http.Request) {
 	email := r.FormValue("email")
 	password := r.FormValue("p1")
 
-	// запустить python скрипт с таймаутом
-	ctx, cancel := context.WithTimeout(context.Background(), scanTimeout)
-	defer cancel()
+	signInDB(email, password)
 
-	cmd := exec.CommandContext(ctx, pythonBin, scriptPath2, email, password)
-	out, err := cmd.CombinedOutput()
+	/*
+		// запустить python скрипт с таймаутом
+		ctx, cancel := context.WithTimeout(context.Background(), scanTimeout)
+		defer cancel()
 
-	fmt.Printf("sign in output:\n%s\n", string(out))
+		cmd := exec.CommandContext(ctx, pythonBin, scriptPath2, email, password)
+		out, err := cmd.CombinedOutput()
 
-	// подготовить код возврата
-	exitCode := 0
-	if err != nil {
-		if ee, ok := err.(*exec.ExitError); ok {
-			exitCode = ee.ExitCode()
-		} else {
-			http.Error(w, "failed to run scanner: "+err.Error(), http.StatusInternalServerError)
-			return
+		fmt.Printf("sign in output:\n%s\n", string(out))
+
+		// подготовить код возврата
+		exitCode := 0
+		if err != nil {
+			if ee, ok := err.(*exec.ExitError); ok {
+				exitCode = ee.ExitCode()
+			} else {
+				http.Error(w, "failed to run scanner: "+err.Error(), http.StatusInternalServerError)
+				return
+			}
+		} else if cmd.ProcessState != nil {
+			exitCode = cmd.ProcessState.ExitCode()
 		}
-	} else if cmd.ProcessState != nil {
-		exitCode = cmd.ProcessState.ExitCode()
-	}
 
-	// Формируем ответ — возвращаем stdout (ожидаем JSON от скрипта)
-	resp := map[string]any{
-		"exit_code": exitCode,
-		"output":    string(out),
-	}
-	//fmt.Println(string(out))
-	w.Header().Set("Content-Type", "application/json")
-	json.NewEncoder(w).Encode(resp)
+		// Формируем ответ — возвращаем stdout (ожидаем JSON от скрипта)
+		resp := map[string]any{
+			"exit_code": exitCode,
+			"output":    string(out),
+		}
+		//fmt.Println(string(out))
+		w.Header().Set("Content-Type", "application/json")
+		json.NewEncoder(w).Encode(resp)*/
 }
 
->>>>>>> 67b6e45 (add js sign in btn and logic without adding cookie)
 // scanHandler: принимает multipart form field "file", сохраняет временно, вызывает python скрипт,
 // ждёт результата и возвращает JSON с { exit_code, output }
 func scanHandler(w http.ResponseWriter, r *http.Request) {
